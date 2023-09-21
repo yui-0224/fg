41becf4325d55b3d308689136f79b8367362a5d3
yui-0224 <144981005+yui-0224@users.noreply.github.com>
2023-09-21 04:38:24 +0000

    ｄ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import random
 st.title("おみくじアプリ")
-ifst.botton("おみくじを引く"):
+if　st.botton("おみくじを引く"):
     results=["大吉","中吉","小吉","吉","凶","大凶"]
     result=randum.choice(results)
     st.write(f"結果{result}")
